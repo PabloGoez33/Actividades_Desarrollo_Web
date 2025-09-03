6a3114f18f1ef4806d0eb7a5bd54dca11f704101
PabloGoez33 <luispablogoez@gmail.com>
2025-09-02 23:05:17 -0500

    Entrega actividad 3 - Home en insta pic
    Agregue el componente Home y arregle el footer y el header

---
+++ Actividad_3_InstaPIC/udemed_202502-main/udemed_202502-main/Talleres/ANGULAR/insta-pic-app/src/app/features/pages/sign-up/sign-up.ts
@@ -0,0 +1,57 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './sign-up.html',
+  styleUrl: './sign-up.css'
+})
+export class SignUp {
+
+  fb = inject(FormBuilder);
+  router = inject(Router);
+
+  ruta = '';
+
+  title = 'Registro de usuario';
+
+  validators = [Validators.required, Validators.minLength(4)];
+
+  signUpForm = this.fb.group({
+    username:['jjzapata', [Validators.required]],
+    email:['', [Validators.required]],
+    password:['', this.validators],
+    rePassword:['',  this.validators],
+  })
+
+
+  onSignUp(){
+    if(!this.signUpForm.valid){
+      alert('Faltan campos por diligenciar');
+      return;
+    }
+    let user = this.signUpForm.value;
+    console.log(user);
+
+    if(user.password !== user.rePassword){
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    if(localStorage.getItem(user.username!)){
+      alert('Usuario ya existe');
+      return;
+    }
+
+    localStorage.setItem(user.username!, JSON.stringify(user));
+    alert('Usuario registrado exitosamente');
+
+    this.router.navigate(['/login']);
+
+    //let user2 = JSON.parse(JSON.stringify(user))
+
+  }
+
+}
